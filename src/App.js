afe17b9b256cb6cd26978a43051ca6d160861f0c
anjalinemalikanti5-art <anjalinemalikanti5@gmail.com>
2026-02-25 15:27:55 +0530

    updated project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-function App()
-  
-    {
-        return(
-          <>
-          <BrowserRouter>
-          <Routes>
-            <Route path="/reg" element={<Reg/>}/>
-            <Route path="/h" element={<Home/>}/>
-            </Routes>
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Reg from "./Reg";
+import Home from "./Home";
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/reg" element={<Reg />} />
+                    <Route path="/h" element={<Home />} />
+                </Routes>
             </BrowserRouter>
-          </>
-        )
-    }
-export default App
+        </>
+    )
+}
+
+export default App;
