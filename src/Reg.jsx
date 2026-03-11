1aa63d2ce54cdf57df2a63bc3ea242dd6dc4dcab
anjalinemalikanti5-art <anjalinemalikanti5@gmail.com>
2026-03-11 12:02:07 +0530

    Refactor Reg component for improved readability

--- src/Reg.jsx
+++ src/Reg.jsx
@@ -3,49 +3,38 @@ import {useState} from "react"
 
 function Reg(){
 
-const[data,setdata]=useState({
-username:"",
-email:"",
-password:""
-})
+    const[data,setdata]=useState({
+        username:"",
+        email:"",
+        password:""
+    })
 
-const changeName=(e)=>{
-setdata({...data,[e.target.name]:e.target.value})
-}
+    const changeName=(e)=>{
+        setdata({...data,[e.target.name]:e.target.value})
+    }
 
-const submit= async()=>{
-try{
+    const submit= async()=>{
+        try{
+            const res = await axios.post(
+                "https://new.onrender.com/register",
+                data
+            )
+            alert("Register Successfully")
+        }
+        catch(xyz){
+            alert("Registration Failed")
+        }
+    }
 
-const res = await axios.post(
-"https://new.onrender.com/register",
-data
-)
-
-alert("Register Successfully")
-
-}
-catch(xyz){
-
-console.log(xyz)
-alert("Reg Failed")
-
-}
-}
-
-return(
-<>
-<h1>iam App</h1>
-
-<input onChange={changeName} name="username" placeholder="enter username"/>
-
-<input onChange={changeName} name="email" placeholder="enter email"/>
-
-<input onChange={changeName} name="password" placeholder="enter password"/>
-
-<button onClick={submit}>register</button>
-
-</>
-)
+    return(
+        <>
+        <h1>iam App</h1>
+        <input onChange={changeName} name="username" placeholder="enter username"/>
+        <input onChange={changeName} name="email" placeholder="enter email"/>
+        <input onChange={changeName} name="password" placeholder="enter password"/>
+        <button onClick={submit}>register</button>
+        </>
+    )
 }
 
 export default Reg
